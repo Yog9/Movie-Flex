74c7bd037b748757099fa091e400b85c8614320e
Yog9 <veryogita@gmail.com>
2019-07-02 13:17:10 +0530

    Updates

---
+++ precache-manifest.bc41fd2c30426db50e72981b7d6f8ec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367f823187ffe5f084a7f7cb8a08e555",
+    "url": "/Movie-Flex/index.html"
+  },
+  {
+    "revision": "f4f81bf0a387e8aa5441",
+    "url": "/Movie-Flex/static/css/main.3bbe6909.chunk.css"
+  },
+  {
+    "revision": "84961e3ba3ac0c7a5d1e",
+    "url": "/Movie-Flex/static/js/2.bda05b80.chunk.js"
+  },
+  {
+    "revision": "f4f81bf0a387e8aa5441",
+    "url": "/Movie-Flex/static/js/main.cde095f5.chunk.js"
+  },
+  {
+    "revision": "c26a31e2f09fbbe0329b",
+    "url": "/Movie-Flex/static/js/runtime~main.b87a8ca0.js"
+  },
+  {
+    "revision": "fc9beeaac7cd8ccbc73046502a321775",
+    "url": "/Movie-Flex/static/media/popcorn.fc9beeaa.svg"
+  }
+]);
